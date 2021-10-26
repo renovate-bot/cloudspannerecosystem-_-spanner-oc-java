f0a5d5c3b99fca091f8583c513c2bd1a1e667dfb
Kiranmayi Bhamidimarri <kiranmayib@google.com>
2021-10-26 08:53:26 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Spanner - OpenCensus Example
+
+## About Cloud Spanner
+
+[Cloud Spanner](https://cloud.google.com/spanner/) is a fully managed, mission-critical,
+relational database service that offers transactional consistency at global scale,
+schemas, SQL (ANSI 2011 with extensions), and automatic, synchronous replication
+for high availability.
+
+Be sure to activate the Cloud Spanner API on the Developer's Console to
+use Cloud Spanner from your project.
+
+See the [Spanner client lib docs](https://googleapis.dev/java/google-cloud-clients/latest/index.html?com/google/cloud/spanner/package-summary.html) to learn how to
+interact with Cloud Spanner using this Client Library.
+
+### Prerequisites
+Please refer to the [getting
+started](https://cloud.google.com/spanner/docs/getting-started/java/) guide.
+
+### Authentication
+See the [Authentication](https://github.com/googleapis/google-cloud-java#authentication) section in the base directory's README.
+
+### To build the example
+Create a table named 'Players' in your database.
+```sql
+CREATE TABLE Players (
+	id STRING(30) NOT NULL,
+	name STRING(30),
+	email STRING(30),
+) PRIMARY KEY (id);
+```
+You may also populate it with a sample record.
+```sql
+INSERT INTO Players (id,name,email)
+VALUES ('a','a','a@a.com');
+```
+Update the instance and database names in PersonController.java to your instance and database.
+
+```bash
+$ mvn clean package
+```
+
+### To Run the example
+```bash
+$ mvn exec:java -Dexec.mainClass=com.example.spanner.App
+```
+
+Go to http://localhost:8080/spanner/ and, start sending read request.
+
+## OpenCensus agent
+
+The `ocagent` can be run directly from sources, binary, or a Docker image. Here will try from the sources.
+
+### Cloning source
+Open up a new shell to get the OpenCensus agent
+```bash
+git clone https://github.com/census-instrumentation/opencensus-service.git && cd opencensus-service
+```
+
+### Building it
+```bash
+make agent
+```
+which will place the binary in the bin folder in your current working directory
+
+### Running it
+```bash
+./bin/ocagent_darwin --config=/conf/config.yaml
+```
+
---
+++ src/main/java/com/example/spanner/PersonController.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.spanner;
+
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.KeySet;
+import com.google.cloud.spanner.Mutation;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.spi.v1.SpannerRpcViews;
+import io.opencensus.contrib.observability.ready.util.BasicSetup;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/spanner")
+public class PersonController {
+  private Spanner spanner;
+  private DatabaseClient dbClient;
+  String instanceId = "demo-instance";
+  String databaseId = "demo-database";
+  String table = "Players";
+
+  PersonController() {
+    // Instantiate the client.
+    SpannerOptions options = SpannerOptions.getDefaultInstance();
+    spanner = options.getService();
+    // And then create the Spanner database client.
+    String projectId = options.getProjectId();
+    dbClient = spanner.getDatabaseClient(DatabaseId.of(projectId, instanceId, databaseId));
+
+    BasicSetup.enableOpenCensus();
+    SpannerRpcViews.registerGfeLatencyAndHeaderMissingCountViews();
+  }
+
+  @GetMapping(path = "/", produces = "application/json")
+  public List<Person> getPerson() {
+    List<Person> list = new ArrayList<>();
+    try (ResultSet resultSet =
+        dbClient
+            .singleUse()
+            .read(
+                table,
+                KeySet.all(), // Read all rows in a table.
+                Arrays.asList("id", "name", "email"))) {
+      while (resultSet.next()) {
+        Person person = new Person();
+        person.setId(resultSet.getString(0));
+        person.setName(resultSet.getString(1));
+        person.setEmail(resultSet.getString(2));
+        list.add(person);
+      }
+    }
+    return list;
+  }
+
+  @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+  public Person addPerson(@RequestBody Person p) {
+    List<Mutation> mutations =
+        Collections.singletonList(
+            Mutation.newInsertBuilder(table)
+                .set("id")
+                .to(UUID.randomUUID().toString())
+                .set("name")
+                .to(p.getName())
+                .set("email")
+                .to(p.getEmail())
+                .build());
+    dbClient.write(mutations);
+    return p;
+  }
+}
